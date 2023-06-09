303fb0e94bb99ef72a5d40d41ed1bac7c4ee1eea
ksharratt <34741754+ksharratt@users.noreply.github.com>
2023-06-09 14:07:16 +1000

    Update config.py with relevant code

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
-# This module handles configuration related tasks such as loading environment variables and setting up the logger.
+# Imports
+import os
+import logging
+
+import json
+
+def load_config():
+    # Load configuration from environment variables
+    config = {
+        "token": os.getenv("BOT_TOKEN"),
+        "prefix": os.getenv("BOT_PREFIX"),
+        "debug": os.getenv("BOT_DEBUG") == "true",
+    }
+
+    # Set up logging
+    logging.basicConfig(level=logging.DEBUG if config["debug"] else logging.INFO)
+
+    return config
